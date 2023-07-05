0628dcbc7d0b68affa53d481ad9c07ffd04b419c
David Chen <dav@Davids-MacBook-Pro.local>
2023-07-05 16:05:49 -0400

    Spline Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Spline from '@splinetool/react-spline';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Spline scene="https://prod.spline.design/NwqqNy6qm8Nea8Yo/scene.splinecode" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
